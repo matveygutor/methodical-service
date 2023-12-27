8fb6acc9aa2c507e5e0cd58e6eb34576ba1a8af0
matveygutor <matweyhutorts@gmail.com>
2023-12-28 01:58:13 +0300

    add SHA256 crypto service

--- MethodicalService/Authentication.xaml.cs
+++ MethodicalService/Authentication.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 
 namespace MethodicalService
@@ -51,15 +55,56 @@ namespace MethodicalService
         {
             try
             {
-                MainWindow window = new MainWindow();
+                using var connection = new SqlConnection(Properties.Resources.connectionString);
+                connection.Open();
+                string hash = connection.Query<string>($"SELECT hash FROM user_identity WHERE login = '{Login}'").SingleOrDefault(); 
+ 
+                byte[] salt = GenerateSalt();
+
+                // создаем Sha256-хеш
+                byte[] sha256Hash = GenerateSha256Hash(Password, salt);
+                string sha256HashString = Convert.ToBase64String(sha256Hash);
                 
-                window.Show();
-                this.Close();
+                if (hash.Equals(sha256HashString))
+                {
+                    string name = connection.Query<string>($"SELECT name + ' ' + fathername FROM user_identity WHERE login = '{Login}'").SingleOrDefault();
+                    MessageBox.Show($"Добро пожаловать, {name}", "Вход в систему", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    MainWindow window = new MainWindow();
+
+                    window.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show($"Неверный логин или пароль", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                    ClearTextBoxes();
+                }
             }
             catch
             {
-                MessageBox.Show("Ошибка при подключении к базе данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Ошибка при подключении к базе данных", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        static byte[] GenerateSha256Hash(string password, byte[] salt)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+
+            SHA256CryptoServiceProvider hash = new();
+
+            return hash.ComputeHash(saltedPassword);
+        }
+        static byte[] GenerateSalt()
+        {
+            const int SaltLength = 64;
+            byte[] salt = new byte[SaltLength];
+
+            var rngRand = new RNGCryptoServiceProvider();
+            rngRand.GetBytes(salt);
+
+            return salt;
         }
 
         public void ClearTextBoxes()
